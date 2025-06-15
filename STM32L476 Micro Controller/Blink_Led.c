45d7edc02aca559b86a5e5f42f1dc71e794ba1c6
Umar Wahid <151420664+umarrao23@users.noreply.github.com>
2025-06-14 21:07:10 -0700

    Update Blink_Led.c

--- STM32L476 Micro Controller/Blink_Led.c
+++ STM32L476 Micro Controller/Blink_Led.c
@@ -45,11 +45,11 @@ Usage:
 #define AHB1_OFFSET 		(0x00020000UL)
 #define AHB1_PERIPH_BASE 	(PERIPH_BASE + AHB1_OFFSET)
 
-#define RCC_OFFSET 			(0x00001000UL)
-#define RCC_BASE			(AHB1_PERIPH_BASE + RCC_OFFSET )
+#define RCC_OFFSET 		(0x00001000UL)
+#define RCC_BASE		(AHB1_PERIPH_BASE + RCC_OFFSET )
 
 #define GPIOA_OFFSET 		(0x000UL)
-#define GPIOA_BASE 			(AHB2_PERIPH_BASE + GPIOA_OFFSET )
+#define GPIOA_BASE 		(AHB2_PERIPH_BASE + GPIOA_OFFSET )
 
 #define AHB2EN_R_OFFSET		(0x4C)
 #define RCC_AHB2EN_R		(*(volatile unsigned int*)(RCC_BASE + AHB2EN_R_OFFSET)) ///clock enabled
@@ -57,13 +57,13 @@ Usage:
 #define MODE_R_OFFSET		(0x00)
 #define GPIOA_MODE_R		(*(volatile unsigned int*)(GPIOA_BASE + MODE_R_OFFSET))
 
-#define OD_R_OFFSET			(0x14)
-#define GPIOA_OD_R			(*(volatile unsigned int*)(GPIOA_BASE + OD_R_OFFSET))
+#define OD_R_OFFSET		(0x14)
+#define GPIOA_OD_R		(*(volatile unsigned int*)(GPIOA_BASE + OD_R_OFFSET))
 
-#define PIN_5				(1<<5)
-#define LED_PIN				PIN_5
+#define PIN_5			(1<<5)
+#define LED_PIN			PIN_5
 
-#define GPIOAEN				(1U<<0) // 0b 0000 0000 0000 0000 0000 0000 0000 0001
+#define GPIOAEN			(1U<<0) // 0b 0000 0000 0000 0000 0000 0000 0000 0001
 
 
 int main(void)
